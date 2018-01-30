50dc1a1a03ae5e2921f7536006ee9b95613b10a3
abhinath84 <abhi.nath@outlook.com>
2018-01-30 17:28:05 +0530

    Updated Spreadsheet class for extend slot for menus.

--- chap04/Spreadsheet/CGSpreadsheet.cpp
+++ chap04/Spreadsheet/CGSpreadsheet.cpp
@@ -1,4 +1,5 @@
 #include <QtGui>
+#include <QApplication>
 
 #include "CGCell.h"
 #include "CGSpreadsheet.h"
@@ -93,4 +94,211 @@ void CGSpreadsheet::somethingChanged()
         recalculate();
 
     emit modified();
+}
+
+bool CGSpreadsheet::writeFile(const QString &fileName)
+{
+    QFile file(fileName);
+    if (!file.open(QIODevice::WriteOnly))
+    {
+        QMessageBox::warning(this, tr("Spreadsheet"),
+                                tr("Cannot write file %1:\n%2.")
+                                .arg(file.fileName())
+                                .arg(file.errorString()));
+
+        return false;
+    }
+
+    QDataStream out(&file);
+    out.setVersion(QDataStream::Qt_5_10);
+
+    out << quint32(MagicNumber);
+
+    QApplication::setOverrideCursor(Qt::WaitCursor);
+    for (int row = 0; row < RowCount; ++row)
+    {
+        for (int column = 0; column < ColumnCount; ++column)
+        {
+            QString str = formula(row, column);
+            if (!str.isEmpty())
+                out << quint16(row) << quint16(column) << str;
+        }
+    }
+    QApplication::restoreOverrideCursor();
+
+    return true;
+}
+
+bool CGSpreadsheet::readFile(const QString &fileName)
+{
+    QFile file(fileName);
+    if (!file.open(QIODevice::ReadOnly))
+    {
+        QMessageBox::warning(this, tr("Spreadsheet"),
+                                tr("Cannot read file %1:\n%2.")
+                                .arg(file.fileName())
+                                .arg(file.errorString()));
+        return false;
+    }
+
+    QDataStream in(&file);
+    in.setVersion(QDataStream::Qt_5_10);
+
+    quint32 magic;
+    in >> magic;
+    if (magic != MagicNumber)
+    {
+        QMessageBox::warning(this, tr("Spreadsheet"),
+                                tr("The file is not a Spreadsheet file."));
+
+        return false;
+    }
+
+    clear();
+
+    quint16 row;
+    quint16 column;
+    QString str;
+
+    QApplication::setOverrideCursor(Qt::WaitCursor);
+    while (!in.atEnd())
+    {
+        in >> row >> column >> str;
+        setFormula(row, column, str);
+    }
+    QApplication::restoreOverrideCursor();
+
+    return true;
+}
+
+void CGSpreadsheet::cut()
+{
+    copy();
+    del();
+}
+
+void CGSpreadsheet::copy()
+{
+    QString str;
+    QTableWidgetSelectionRange range = selectedRange();
+
+    for(int i = 0; i < range.rowCount(); ++i)
+    {
+        if(i > 0)
+            str +="\n";
+
+        for(int j = 0; j < range.columnCount(); ++j)
+        {
+            if(j > 0)
+                str +="\t";
+
+            str +=formula(range.topRow() + i, range.leftColumn() + j);
+        }
+    }
+
+    QApplication::clipboard()->setText(str);
+}
+
+QTableWidgetSelectionRange CGSpreadsheet::selectedRange() const
+{
+    QList<QTableWidgetSelectionRange> ranges = selectedRanges();
+    if(ranges.isEmpty())
+        return QTableWidgetSelectionRange();
+
+    return ranges.first();
+}
+
+void CGSpreadsheet::paste()
+{
+    QTableWidgetSelectionRange range = selectedRange();
+    QString str = QApplication::clipboard()->text();
+    QStringList rows = str.split(’\n’);
+    int numRows = rows.count();
+    int numColumns = rows.first().count(’\t’) + 1;
+
+    if (range.rowCount() * range.columnCount() != 1
+        && (range.rowCount() != numRows
+            || range.columnCount() != numColumns))
+    {
+        QMessageBox::information(this, tr("Spreadsheet"),
+                                    tr("The information cannot be pasted because the copy "
+                                    "and paste areas aren’t the same size."));
+        return;
+    }
+
+    for (int i = 0; i < numRows; ++i)
+    {
+        QStringList columns = rows[i].split(’\t’);
+        for (int j = 0; j < numColumns; ++j)
+        {
+            int row = range.topRow() + i;
+            int column = range.leftColumn() + j;
+            if (row < RowCount && column < ColumnCount)
+                setFormula(row, column, columns[j]);
+        }
+    }
+    somethingChanged();
+}
+
+void CGSpreadsheet::del()
+{
+    foreach (QTableWidgetItem *item, selectedItems())
+        delete item;
+}
+
+void CGSpreadsheet::selectCurrentRow()
+{
+    selectRow(currentRow());
+}
+void CGSpreadsheet::selectCurrentColumn()
+{
+    selectColumn(currentColumn());
+}
+
+void CGSpreadsheet::findNext(const QString &str, Qt::CaseSensitivity cs)
+{
+    int row = currentRow();
+    int column = currentColumn() + 1;
+    while (row < RowCount)
+    {
+        while (column < ColumnCount)
+        {
+            if (text(row, column).contains(str, cs))
+            {
+                clearSelection();
+                setCurrentCell(row, column);
+                activateWindow();
+
+                return;
+            }
+            ++column;
+        }
+        column = 0;
+        ++row;
+    }
+    QApplication::beep();
+}
+
+void CGSpreadsheet::findPrevious(const QString &str, Qt::CaseSensitivity cs)
+{
+    int row = currentRow();
+    int column = currentColumn() - 1;
+    while (row >= 0)
+    {
+        while (column >= 0)
+        {
+            if (text(row, column).contains(str, cs))
+            {
+                clearSelection();
+                setCurrentCell(row, column);
+                activateWindow();
+
+                return;
+            }
+            --column;
+        }
+        column = ColumnCount - 1;
+        --row;
+    }
+    QApplication::beep();
 }
