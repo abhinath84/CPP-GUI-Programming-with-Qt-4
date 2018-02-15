fe996951e7d31daccb508cf973e325a3843510d4
abhinath84 <abhi.nath@outlook.com>
2018-02-15 17:20:45 +0530

    Updated CGIconEditor class.

--- chap05/CGIconEditor/main.cpp
+++ chap05/CGIconEditor/main.cpp
@@ -8,7 +8,12 @@ int main(int argc, char *argv[])
 
     CGIconEditor iconEditor;
     iconEditor.setWindowTitle(QObject::tr("Icon Editor"));
-    iconEditor.setIconImage(QImage(":/images/mouse.png"));
+    iconEditor.resize(250, 250);
+
+    // "/run/media/anath/data/anath/myproj/QT/C++ GUI Programming with Qt 4/chap05/build-IconEditor-Desktop_Qt_5_10_0_GCC_64bit-Debug/images/mouse.png"
+    QImage img(":/images/mouse.png");
+
+    iconEditor.setIconImage(img);
     iconEditor.show();
 
     return app.exec();
